e631e91cef06c2e019ff472e8704157c915cf330
nabin-nath <nabinnath9@gmail.com>
2021-07-18 11:34:32 +0530

    movie db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie.jsx";
+import Loading from "./components/Loading";
+import Home from "./components/Home.js";
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Notfound from "./components/Notfound.js";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home />
   );
 }
 
