3587373eee393f9c70044d6a1fe5adeaa507c471
nabin-nath <nabinnath9@gmail.com>
2021-07-18 11:48:39 +0530

    dep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React, { useEffect, useState } from "react";
-import Movie from "./components/Movie.jsx";
-import Loading from "./components/Loading";
 import Home from "./components/Home.js";
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import Notfound from "./components/Notfound.js";
 
 function App() {
 
